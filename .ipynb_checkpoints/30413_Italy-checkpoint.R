553917a16204296e7cbd5e1f27e5244d77a55fbe
AlessandroMorosini <morosini.alessandro@outlook.com>
2023-03-23 13:58:13 +0100

    process fundamental raw data

---
+++ .ipynb_checkpoints/30413_Italy-checkpoint.R
@@ -0,0 +1,11 @@
+##### 30413 ECONOMETRICS ASSIGNMENT Y22/23 ####
+
+# Alessandro Morosini - 3149076
+# Francesco Vacca - 
+# Tancredi Dorsi - 
+
+
+# Real exchange rate 
+# Terms of trade
+# Foreign output 
+# Foreign interest rate 
---
+++ 30413_Italy.R
@@ -0,0 +1,13 @@
+##### 30413 ECONOMETRICS ASSIGNMENT Y22/23 ####
+
+# Alessandro Morosini - 3149076
+# Francesco Vacca - 
+# Tancredi Dorsi - 
+
+# installing / importing libraries
+# install.packages('tseries')
+# install.packages('tidyverse')
+library(ggplot2)
+library(lmtest)
+library(tseries)
+library(tidyverse)
