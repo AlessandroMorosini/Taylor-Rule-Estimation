0aa786839199884453c6f1306dfb0618e836e8cf
francescoovacca <102154214+francescoovacca@users.noreply.github.com>
2023-03-23 14:11:20 +0100

    add ids

--- 30413_Italy.R
+++ 30413_Italy.R
@@ -1,8 +1,8 @@
 ##### 30413 ECONOMETRICS ASSIGNMENT Y22/23 ####
 
 # Alessandro Morosini - 3149076
-# Francesco Vacca - 
-# Tancredi Dorsi - 
+# Francesco Vacca - 3140929
+# Tancredi Dorsi - 3161375
 
 # installing / importing libraries
 # install.packages('tseries')
